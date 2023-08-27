89c372508648752c054d780e6fc3d248751eae33
ZerGo0 <18653821+ZerGo0@users.noreply.github.com>
2023-08-27 21:01:32 +0200

    Commented nvim-ufo

--- config.lua
+++ config.lua
@@ -285,26 +285,26 @@ lvim.plugins = {
       dir = vim.fn.expand(vim.fn.stdpath "config" .. "/session/"),
     }
   },
-  {
-    "kevinhwang91/nvim-ufo",
-    dependencies = {
-      "kevinhwang91/promise-async"
-    },
-    config = function()
-      require('ufo').setup({
-        provider_selector = function(bufnr, filetype, buftype)
-          return { 'treesitter', 'indent' }
-        end
-      })
-    end,
-    init = function()
-      vim.o.foldcolumn = '1' -- '0' is not bad
-      vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
-    end,
-    lazy = false
-  }
+  -- {
+  --   "kevinhwang91/nvim-ufo",
+  --   dependencies = {
+  --     "kevinhwang91/promise-async"
+  --   },
+  --   config = function()
+  --     require('ufo').setup({
+  --       provider_selector = function(bufnr, filetype, buftype)
+  --         return { 'treesitter', 'indent' }
+  --       end
+  --     })
+  --   end,
+  --   init = function()
+  --     vim.o.foldcolumn = '1' -- '0' is not bad
+  --     vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+  --     vim.o.foldlevelstart = 99
+  --     vim.o.foldenable = true
+  --   end,
+  --   lazy = false
+  -- }
 }
 
 -- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
